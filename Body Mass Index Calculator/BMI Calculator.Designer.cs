2618ffedd344a4b25870f441263d5920e3ada73a
carole bacon <carolelynnbacon@gmail.com>
2015-02-17 13:21:21 -0800

    C# BMI Calculator

---
+++ Body Mass Index Calculator/BMI Calculator.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Body_Mass_Index_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.bmiLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.bmiTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightLabel.Location = new System.Drawing.Point(64, 47);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(72, 24);
+            this.heightLabel.TabIndex = 0;
+            this.heightLabel.Text = "Height";
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightLabel.Location = new System.Drawing.Point(64, 95);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(76, 24);
+            this.weightLabel.TabIndex = 2;
+            this.weightLabel.Text = "Weight";
+            // 
+            // bmiLabel
+            // 
+            this.bmiLabel.AutoSize = true;
+            this.bmiLabel.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmiLabel.Location = new System.Drawing.Point(64, 235);
+            this.bmiLabel.Name = "bmiLabel";
+            this.bmiLabel.Size = new System.Drawing.Size(102, 24);
+            this.bmiLabel.TabIndex = 6;
+            this.bmiLabel.Text = "Your BMI";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(68, 159);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(125, 40);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(349, 159);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 40);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click_1);
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightTextBox.Location = new System.Drawing.Point(349, 47);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 32);
+            this.heightTextBox.TabIndex = 1;
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightTextBox.Location = new System.Drawing.Point(349, 95);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(100, 32);
+            this.weightTextBox.TabIndex = 3;
+            // 
+            // bmiTextBox
+            // 
+            this.bmiTextBox.Font = new System.Drawing.Font("Californian FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmiTextBox.Location = new System.Drawing.Point(349, 235);
+            this.bmiTextBox.Name = "bmiTextBox";
+            this.bmiTextBox.Size = new System.Drawing.Size(100, 32);
+            this.bmiTextBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 328);
+            this.Controls.Add(this.bmiTextBox);
+            this.Controls.Add(this.weightTextBox);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.bmiLabel);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.heightLabel);
+            this.Name = "Form1";
+            this.Text = "BMI Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.Label bmiLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.TextBox bmiTextBox;
+    }
+}
+
---
+++ Body Mass Index Calculator/BMI Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Body_Mass_Index_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            /* Calculates a person's Body Mass Index */
+
+            int heightText;  // The person's height in inches
+            int weightText;   // The person's weight in pounds
+            int bmiText;    // The person's Body Mass Index
+
+            // Get the person's height in inches
+            heightText = int.Parse(heightTextBox.Text);
+
+            // Get the person's weight in pounds
+            weightText = int.Parse(weightTextBox.Text);
+
+            // Calculate the (height * height) / the weight * 703)
+            bmiText = (weightText * 703) / (heightText * heightText);
+          
+            // Display Body Mass Index
+            bmiTextBox.Text = bmiText.ToString();
+
+        }
+
+       
+
+        private void clearButton_Click_1(object sender, EventArgs e)
+        {
+            /* Clears text boxes and output label */
+
+            heightTextBox.Text = "";
+            weightTextBox.Text = "";
+            bmiTextBox.Text = "";
+        }
+    }
+}
